32c722248a502a2d41e5c4932087dd7f023c9c98
yymato <070913a@gmail.com>
2025-04-29 23:53:29 +0300

    Начало работы по распарсингу xls таблиц в бд

---
+++ Работа с excel таблицей/excel.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import requests
+
+
+def unpack_orders_xls(table):  # TODO: Доделать функцию, которая вытягивает данные с xls таблицы и кидает их в бд
+    server = 'http://localhost:5000/api/orders'
+
+    df = pd.read_excel(table)
+    data_tuples = [tuple(x) for x in df.values]
+    for analytics_id, address, name, phone, price in data_tuples:
+        data = {
+            'phone': phone,
+            'name': name,
+            'address': address,
+            'analytics_id': analytics_id,
+            'price': price
+        }
+        response = requests.get(server, params=data)
+        if not response:
+            raise Exception('REQUEST FAILED')
